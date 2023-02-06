c66805ee91a53e1fa1b3bab1e65cdbe1a64f8461
hasu_non_sucre <hasu_m.s@icloud.com>
2023-02-07 03:05:18 +0900

    update: サイズを固定にしてpc用のトップ仮完成

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,36 +12,38 @@ export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <div className={cx(styles.card, styles.flex)}>
-          <div className={styles.leftCol}>
-            <div className={styles.profile}>
-              <div className={styles.profileImg_base}>
-                <div className={styles.profileImg}>
-                  <Image src={profile} layout="fill" objectFit="contain" />
+        <div className={styles.card}>
+          <div className={styles.contents}>
+            <div className={styles.leftCol}>
+              <div className={styles.profile}>
+                <div className={styles.profileImg_base}>
+                  <div className={styles.profileImg}>
+                    <Image src={profile} layout="fill" objectFit="contain" />
+                  </div>
                 </div>
+                <p className={styles.profileName}>Muto Sota</p>
               </div>
-              <p className={styles.profileName}>Muto Sota</p>
             </div>
-          </div>
 
-          <div className={styles.rightCol}>
-            <ul className={styles.list}>
-              <li className={styles.item}>
-                <Link href="#" className={styles.link}>
-                  - About
-                </Link>
-              </li>
-              <li className={styles.item}>
-                <Link href="#" className={styles.link}>
-                  - Work
-                </Link>
-              </li>
-              <li className={styles.item}>
-                <Link href="#" className={styles.link}>
-                  - Contact
-                </Link>
-              </li>
-            </ul>
+            <div className={styles.rightCol}>
+              <ul className={styles.list}>
+                <li className={styles.item}>
+                  <Link href="#" className={styles.link}>
+                    - About
+                  </Link>
+                </li>
+                <li className={styles.item}>
+                  <Link href="#" className={styles.link}>
+                    - Work
+                  </Link>
+                </li>
+                <li className={styles.item}>
+                  <Link href="#" className={styles.link}>
+                    - Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </main>
